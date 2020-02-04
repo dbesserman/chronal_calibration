5fe1246cf6c43fbb76a7fd7829a648af1e4b3fbf
David Besserman <d.besserman@group-label.com>
2020-02-04 13:47:37 +0100

    [refactor] minor test refactor

--- test/chronal_calibration_test.exs
+++ test/chronal_calibration_test.exs
@@ -4,17 +4,17 @@ defmodule ChronalCalibrationTest do
 
   describe "compute_frequency/2" do
     test "adds the sum of the variations taken from a file to the inital frequency when the file's path is passed in" do
-      assert ChronalCalibration.computed_frequency(0, "./test/sample.data") == 3
+      assert ChronalCalibration.computed_frequency(0, "./test/data/sample.data") == 3
     end
   end
 
   describe "first_frequency_repeated/2" do
     test "returns the first computed_frequency repeated twice if a frequency is repeated" do
-      assert ChronalCalibration.first_frequency_repeated(0, "./test/sample.data") == 3
+      assert ChronalCalibration.first_frequency_repeated(0, "./test/data/sample.data") == 3
     end
 
     test "loops data variations untill a frequency is found" do
-      assert ChronalCalibration.first_frequency_repeated(0, "./test/sample_requiring_second_loop.data") == 0
+      assert ChronalCalibration.first_frequency_repeated(0, "./test/data/sample_requiring_second_loop.data") == 0
     end
   end
 end
